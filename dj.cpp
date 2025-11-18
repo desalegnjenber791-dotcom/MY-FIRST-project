9de846e53ccfc3a56986121209c5033a1c144e4d
desalegnjenber791-dotcom <desalegnjenber791@gmail.com>
2025-11-18 21:37:47 +0300

    Fix syntax errors in multiplication program

--- dj.cpp
+++ dj.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
 int x=5;
-int y=8;
-int=mul=x*y;
+int y = 8;
+int mul=x*y;
 cout<<"multiplication is :" <<mul<<endl;
     return 0;
 }
+
